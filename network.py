4bab53d077f8f3c6e4c5e9300a63ddc92a7ec6a9
Colin Campbell <colincampbell321123@hotmail.com>
2020-06-06 16:24:58 +0100

    Represent layers as an object

--- network.py
+++ network.py
@@ -1,30 +1,30 @@
 import numpy as np
 
+np.random.seed(0)
+
 # input (batch of)
-inputs = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, 0.8]]
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, 0.8]]
 
-# layer
-weights = [
-    [0.2, 0.8, -0.5, 1.0],
-    [0.5, -0.91, 0.26, -0.5],
-    [-0.26, -0.27, 0.17, 0.87]
-]
-biases = [2, 3, 0.5]
 
-# output - layer2 input
-layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10*np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-# layer2
-weights2 = [
-    [0.1, -0.14, 0.5],
-    [-0.5, 0.12, -0.33],
-    [-0.44, 0.73, -0.13]
-]
-biases2 = [-1, 2, -0.5]
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+# layers
+layer1 = Layer_Dense(4, 5)
+
+layer1.forward(X)
+
+layer2 = Layer_Dense(5, 2)
+
+layer2.forward(layer1.output)
 
 # output
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
-
-print(layer2_outputs)
+print(layer2.output)
