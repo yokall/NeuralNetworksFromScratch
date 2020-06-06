0cc48950c46c6055177350f216720066bb038138
Colin Campbell <colincampbell321123@hotmail.com>
2020-06-06 15:55:51 +0100

    Take a batch of inputs rather than single snapshot

--- network.py
+++ network.py
@@ -1,6 +1,11 @@
 import numpy as np
 
-inputs = [1, 2, 3, 2.5]
+# input (batch of)
+inputs = [[1, 2, 3, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, 0.8]]
+
+# layer
 weights = [
     [0.2, 0.8, -0.5, 1.0],
     [0.5, -0.91, 0.26, -0.5],
@@ -8,6 +13,7 @@ weights = [
 ]
 biases = [2, 3, 0.5]
 
-output = np.dot(weights, inputs) + biases
+# output
+output = np.dot(inputs, np.array(weights).T) + biases
 
 print(output)
